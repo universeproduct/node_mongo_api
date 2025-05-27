f339227d1ee9094610c2b4cfc4184b1467a9d2d6
Santhoshkumar52 <santhosh5002kumar@gmail.com>
2025-05-27 12:43:41 +0530

    Upgraded with Express and Installed other packages

--- node/app.js
+++ node/app.js
@@ -1,8 +1,17 @@
-const http=require('http');
-const server=http.createServer((req,res)=>{
-    res.writeHead(200,{'content-Type':'text/plain'});
-    res.end("hello");
-})
-server.listen(5000,()=>{
-    console.log("server listening")
-})
+import express from 'express' // express
+import dotenv from 'dotenv' // environment variables
+import mongoose from 'mongoose' // database
+import cors from 'cors' // cors policy
+
+// =========================================== Import Package Ends here =============================
+
+dotenv.config()
+const app= express() 
+const portNo=process.env.PORT // port number
+
+// =========================================== Configuration Ends here =============================
+
+app.get("/",(req,res)=>{
+    res.send("Hello World !")
+}).listen(portNo,()=>{console.info(`Server Started in http://localhost:${portNo}/`)})
+// =========================================== Server Startup Ends Here =============================
