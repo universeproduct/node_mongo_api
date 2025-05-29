5f4504cfe543441dd3c959a5e364d1bea7e54efb
subhameena0402 <subhameena0402@gmail.com>
2025-05-29 08:29:32 +0530

    created authentication

--- node/app.js
+++ node/app.js
@@ -2,15 +2,21 @@ import express from 'express' // express
 import dotenv from 'dotenv' // environment variables
 import mongoose from 'mongoose' // database
 import cors from 'cors' // cors policy
+import authRoutes from '../routes/auth.js';
+import protectedRoutes from '../routes/protected.js';
 
 // =========================================== Import Package Ends here =============================
 
 dotenv.config()
 const app= express() 
-const portNo=process.env.PORT // port number
-
-// =========================================== Configuration Ends here =============================
 app.use(express.json());
+const portNo=process.env.PORT || 5000; // port number
+// =========================================== Configuration Ends here =============================
+app.use('/api/auth', authRoutes);
+app.use('/api/protected', protectedRoutes);
+
+//==============================================essential middleware=============================
+
 
 app.get("/",(req,res)=>{ 
     res.send("Hello from express!")
